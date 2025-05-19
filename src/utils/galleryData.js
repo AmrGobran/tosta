7b87dd1e0bd870da384d16ba830e6f94719842e5
Amr Gobran <amrgobran0100@gmail.com>
2025-05-19 15:30:59 +0300

    fix images

--- src/utils/galleryData.js
+++ src/utils/galleryData.js
@@ -1,32 +1,32 @@
 export const galleryImages = [
 	{
 		id: 1,
-		src: "tosta/images/gallery/img-1.jpg",
+		src: "../tosta/images/gallery/img-1.jpg",
 		alt: "Coffee shop interior",
 	},
 	{
 		id: 2,
-		src: "tosta/images/gallery/img-2.jpg",
+		src: "../tosta/images/gallery/img-2.jpg",
 		alt: "Barista making latte art",
 	},
 	{
 		id: 3,
-		src: "tosta/images/gallery/img-3.jpg",
+		src: "../tosta/images/gallery/img-3.jpg",
 		alt: "Coffee beans closeup",
 	},
 	{
 		id: 4,
-		src: "tosta/images/gallery/img-4.jpg",
+		src: "../tosta/images/gallery/img-4.jpg",
 		alt: "Coffee and pastry",
 	},
 	{
 		id: 5,
-		src: "tosta/images/gallery/img-5.jpg",
+		src: "../tosta/images/gallery/img-5.jpg",
 		alt: "People enjoying coffee",
 	},
 	{
 		id: 6,
-		src: "tosta/images/gallery/img-6.jpg",
+		src: "../tosta/images/gallery/img-6.jpg",
 		alt: "Coffee brewing process",
 	},
 ];
